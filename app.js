b7ef5cb12239ed4c0e8d19323405731c217d94c9
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-30 14:33:44 +0530

    app.js

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-﻿(function () {
-    'use strict';
+(function () {
+  'use strict';
+  angular.module('MenuApp', ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
 
-    angular.module('MenuApp', ['ui.router'])
-        .config(RoutesConfig);
-
-    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-    function RoutesConfig($stateProvider, $urlRouterProvider) {
-        // Redirect to home if no other URL matches
-        $urlRouterProvider.otherwise('/');
-
-        // *** Set up UI states ***
-        $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'home.template.html'
-            })
-            .state('categories', {
-                url: '/categories',
-                templateUrl: 'categories.template.html',
-                controller: 'CategoriesController as categoriesCtrl',
-                resolve: {
-                    items: ['MenuService', function (MenuService) {
-                        return MenuService.getAllCategories();
-                    }]
-                }
-            })
-            .state('items', {
-                url: '/items/{categoryId}',
-                templateUrl: 'items.template.html',
-                controller: 'ItemsController as itemsCtrl',
-                resolve: {
-                    items: ['$stateParams', 'MenuService',
-                        function ($stateParams, MenuService) {
-                            return MenuService.getItemsForCategory($stateParams.categoryId);
-                        }]
-                }
-            });
-    }
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.template.html'
+      })
+      .state('categories', {
+        url: '/categories',
+        templateUrl: 'categories.template.html',
+        controller: 'CategoriesController as categoriesCtrl',
+        resolve: {
+          items: ['MenuService', function (MenuService) {
+            return MenuService.getAllCategories();
+          }]
+        }
+      })
+      .state('items', {
+        url: '/items/:categoryId',
+        templateUrl: 'items.template.html',
+        controller: 'ItemsController as itemsCtrl',
+        resolve: {
+          items: ['$stateParams', 'MenuService', function ($stateParams, MenuService) {
+            return MenuService.getItemsForCategory($stateParams.categoryId);
+          }]
+        }
+      });
+  });
 })();
