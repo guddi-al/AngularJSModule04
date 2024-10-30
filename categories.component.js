a69337a1c7969565bcc2e5c378ad0029c375b3bb
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-30 15:02:17 +0530

    categories.component.js

--- categories.component.js
+++ categories.component.js
@@ -3,10 +3,14 @@
   angular.module('MenuApp')
     .controller('CategoriesController', CategoriesController);
 
-  CategoriesController.$inject = ['items'];
-  function CategoriesController(items) {
+  CategoriesController.$inject = ['$http'];
+  function CategoriesController($http) {
     var categoriesCtrl = this;
-    categoriesCtrl.items = items;
+    categoriesCtrl.items = [];
+
+    // Fetch categories from a simple static JSON file or API
+    $http.get('categories.json').then(function (response) {
+      categoriesCtrl.items = response.data;
+    });
   }
 })();
-
