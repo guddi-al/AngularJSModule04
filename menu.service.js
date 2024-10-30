6ff2189355c92366e5d3a184ad30225e4c837859
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-30 14:45:29 +0530

    menu.service.js

--- menu.service.js
+++ menu.service.js
@@ -1,31 +1,30 @@
-﻿(function () {
-    'use strict';
+(function () {
+  'use strict';
+  angular.module('MenuApp')
+    .service('MenuService', MenuService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
-    angular.module('MenuApp')
-        .service('MenuService', MenuService)
-        .constant('ApiBasePath', "");
+  MenuService.$inject = ['$http', 'ApiBasePath'];
+  function MenuService($http, ApiBasePath) {
+    var service = this;
 
-    MenuService.$inject = ['$http', 'ApiBasePath'];
-    function MenuService($http, ApiBasePath) {
-        var service = this;
+    service.getAllCategories = function () {
+      return $http({
+        method: "GET",
+        url: (ApiBasePath + "/categories.json")
+      }).then(function (response) {
+        return response.data;
+      });
+    };
 
-        service.getAllCategories = function () {
-            return $http({
-                method: "GET",
-                url: (ApiBasePath + "/categories.json")
-            }).then(function (response) {
-                return response.data;
-            });
-        };
-
-        service.getItemsForCategory = function (categoryShortName) {
-            return $http({
-                method: "GET",
-                url: (ApiBasePath + "/menu_items.json"),
-                params: { category: categoryShortName }
-            }).then(function (response) {
-                return response.data;
-            });
-        };
-    }
+    service.getItemsForCategory = function (categoryShortName) {
+      return $http({
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json"),
+        params: { category: categoryShortName }
+      }).then(function (response) {
+        return response.data;
+      });
+    };
+  }
 })();
