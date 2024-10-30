49e67cf3eaa14be8df61a674f4a707aef3813f33
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-30 15:13:49 +0530

    menu.service.js

--- menu.service.js
+++ menu.service.js
@@ -1,30 +1,38 @@
 (function () {
   'use strict';
   angular.module('MenuApp')
-    .service('MenuService', MenuService)
-    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    .service('MenuService', MenuService);
 
-  MenuService.$inject = ['$http', 'ApiBasePath'];
-  function MenuService($http, ApiBasePath) {
+  function MenuService() {
     var service = this;
 
-    service.getAllCategories = function () {
-      return $http({
-        method: "GET",
-        url: (ApiBasePath + "/categories.json")
-      }).then(function (response) {
-        return response.data;
-      });
+    var categories = [
+      { id: 1, name: 'Appetizers' },
+      { id: 2, name: 'Main Courses' },
+      { id: 3, name: 'Desserts' }
+    ];
+
+    var items = {
+      1: [
+        { name: 'Spring Rolls', description: 'Crispy rolls with veggies.' },
+        { name: 'Dumplings', description: 'Steamed dumplings with pork.' }
+      ],
+      2: [
+        { name: 'Grilled Salmon', description: 'Salmon with a lemon butter sauce.' },
+        { name: 'Steak', description: 'Grilled to perfection.' }
+      ],
+      3: [
+        { name: 'Cheesecake', description: 'Creamy cheesecake with a graham cracker crust.' },
+        { name: 'Ice Cream', description: 'Vanilla ice cream with chocolate sauce.' }
+      ]
     };
 
-    service.getItemsForCategory = function (categoryShortName) {
-      return $http({
-        method: "GET",
-        url: (ApiBasePath + "/menu_items.json"),
-        params: { category: categoryShortName }
-      }).then(function (response) {
-        return response.data;
-      });
+    service.getAllCategories = function () {
+      return Promise.resolve(categories);
+    };
+
+    service.getItemsForCategory = function (categoryId) {
+      return Promise.resolve(items[categoryId]);
     };
   }
 })();
